abaf80be6504f2f4d91ceeae0e4b2ec8d421acf2
Techcomputerworld <techcomputerworld@outlook.com>
2017-08-07 13:59:48 +0200

    creando el disparo

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rig;
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+    
+    void Start () {
+        //eje x transform.right, eje y transform.up, eje z transform.forward 
+        // forward tendremos la velocidad a 1 esta normalizado
+        rig.velocity = transform.forward * speed;
+	}
+	
+	
+}
